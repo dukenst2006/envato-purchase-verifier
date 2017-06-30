75c215670c95ac9149f37210009c9474a5955131
Shameem Reza <codechef@yahoo.com>
2017-06-30 21:02:25 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 $username='admin'; //your login username here
-$password='1234'; //your login password here
+$password='12345'; //your login password here
 $envato_username='shameem'; //your envato login username here
 $envato_api='ssgsgergdfdhfgvncndfxcbxf45dgtw3t'; //your envato api key
 ?>
