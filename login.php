ccf4041f9022af1e93d2e01ba307d475c8b0eb3c
Shameem Reza <codechef@yahoo.com>
2017-06-30 21:06:11 +0600

    Update login.php

--- login.php
+++ login.php
@@ -35,6 +35,7 @@ if(isset($_SESSION['UserData']['Username'])){
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Shameem Reza">
 <title>Envato Purchase Verifier</title>
 
 <!-- Bootstrap -->
